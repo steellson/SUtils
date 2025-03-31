f4b5bbc3469b9c189e6862521234fee704665a40
Andrew Steellson <113777921+steellson@users.noreply.github.com>
2025-03-31 20:58:11 +0300

    add DS

---
+++ Sources/DS/DS.swift
@@ -0,0 +1,44 @@
+//
+//  DS.swift
+//  SUtils
+//
+//  Created by Andrew Steellson on 31.03.2025.
+//
+
+import Foundation
+
+/// `` ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ``
+/// `` ☩   Design System   ☩ ``
+/// `` ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ☩ ``
+
+public enum DS {
+    public enum Spacer {
+        /// 2px
+        public static let small: CGFloat = 2
+        /// 4px
+        public static let base: CGFloat = 4
+        /// 8px
+        public static let X2: CGFloat = 8
+        /// 12px
+        public static let X3: CGFloat = 12
+        /// 16px
+        public static let X4: CGFloat = 16
+        /// 24px
+        public static let X16: CGFloat = 24
+        /// 32px
+        public static let large: CGFloat = 32
+    }
+
+    public enum Radius {
+        /// 4px
+        public static let base: CGFloat = 4
+        /// 8px
+        public static let X2: CGFloat = 8
+        /// 12х
+        public static let X3: CGFloat = 12
+        /// 16px
+        public static let X4: CGFloat = 16
+        /// 24px
+        public static let large: CGFloat = 25
+    }
+}
